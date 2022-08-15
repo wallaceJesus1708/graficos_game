32e4851d955195624cbbd89af5c4364bad52ce42
Wallace Amorim <wallaceamorim18@gmail.com>
2022-08-15 14:31:47 -0300

    Frame, game looping e Render

---
+++ src/graficos/Game.java
@@ -0,0 +1,88 @@
+package graficos;
+
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3640852252549375978L;
+
+	public static JFrame frame;
+	private Thread thread;
+	private boolean isRunning = true;
+	private final int WIDTH = 160;
+	private final int HEIGHT = 120;
+	private final int SCALE = 4;
+	
+	public Game() {
+		setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+		initFrame();	
+	}
+	
+	public void initFrame() {
+		frame = new JFrame("Gráficos com Java");
+		frame.add(this);
+		frame.setResizable(false);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	
+	public synchronized void start() {
+		thread = new Thread(this);
+		isRunning = true;
+		thread.start();
+	}
+	
+	public synchronized void stop() {
+		
+	}
+	
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.start();
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void render() {
+		
+	}
+	
+	@Override
+	public void run() {
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		int frames = 0;
+		double timer = System.currentTimeMillis();
+		while(isRunning) {
+			long now = System.nanoTime();
+			delta+= (now - lastTime) / ns;
+			lastTime = now;
+			if(delta >= 1) {
+				tick();
+				render();
+				frames++;
+				delta--;
+			}
+			
+			if(System.currentTimeMillis() - timer >= 1000) {
+				System.out.println("FPS: "+frames);
+				frames = 0;
+				timer+=1000;
+			}
+		}
+		
+	}
+
+}
